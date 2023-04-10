64e8f891a1a761e6727a6f9085ae350ac66a50a0
Arseid <yvesrobert.estrada@gmail.com>
2023-04-10 21:33:20 +0200

    Favorites page done

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -9,12 +9,39 @@ import {Product} from "../models/product.model";
 })
 export class FavoritesComponent {
   favorites: Product[] = [];
+  search: string = '';
+  sortTitle: string = 'asc';
+  sortDate: string = 'asc';
 
   constructor(private favoritesService: FavoriteProductsService) {
   }
 
   ngOnInit(): void {
     this.favorites = this.favoritesService.getFavorites();
+  }
+
+  onRemoveFromFavorites(product: Product): void {
+    this.favoritesService.removeFromFavorites(product.id);
+    this.favorites = this.favoritesService.getFavorites();
+  }
+
+  toggleSortOrder(sortType: string) {
+    if (sortType === 'title') {
+      if (this.sortTitle==='desc'){
+        this.sortTitle = 'asc';
+      }
+      else {
+        this.sortTitle = 'desc';
+      }
+    }
+    else {
+      if (this.sortDate==='desc'){
+        this.sortDate = 'asc';
+      }
+      else {
+        this.sortDate = 'desc';
+      }
+    }
   }
 
 }
