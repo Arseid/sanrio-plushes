e6322fe36bf45e58fffa0dd2189b024f2b35745c
Arseid <yvesrobert.estrada@gmail.com>
2023-04-10 13:20:04 +0200

    products list but dynamic sorting doesn't work, to finish later

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,18 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-import {Product} from "../models/product.model";
-import {ProductsService} from "../services/product.service";
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
+import { ProductsService } from '../services/product.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+  styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent implements OnInit {
   products!: Product[];
   search: string = '';
+  sortOrder: string = 'desc';
   constructor(private productsService: ProductsService) {}
 
   ngOnInit() {
     this.products = this.productsService.getAllProducts();
+  }
+
+  toggleSortOrder() {
+    if (this.sortOrder==='desc'){
+      this.sortOrder = 'asc';
+    }
+    else {
+      this.sortOrder = 'desc';
+    }
   }
 }
