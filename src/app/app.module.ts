453ca12a964e428fe8a94b3df30aa67f3de9010f
Arseid <yvesrobert.estrada@gmail.com>
2023-04-10 09:22:50 +0200

    about endpoint, product detail endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { ProductsListComponent } from './products-list/products-list.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
     ProductsListComponent,
     HeaderComponent,
     FooterComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    AboutComponent
   ],
     imports: [
         BrowserModule,
