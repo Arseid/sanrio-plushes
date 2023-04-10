34a40f320e0b60fed68743d50bb779d69486c61a
Arseid <yvesrobert.estrada@gmail.com>
2023-04-10 10:12:21 +0200

    Header design + links to other pages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   @Input() title!: string;
